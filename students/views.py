1fd308d45795f5b97a8da6b2da871e69b05aa88f
Samandar0005 <swatushuwu@gmail.com>
2024-11-27 18:54:48 +0300

    I'm Alpha!

--- students/views.py
+++ students/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Student
 
-# Create your views here.
+def student_view(request):
+    first_name = request.GET.get('first_name')
+    last_name = request.GET.get('last_name')
+    email = request.GET.get('email')
+    age = request.GET.get('age')
+    if first_name is not None and last_name is not None and email is not None and age is not None:
+        Student.objects.create(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            age=age
+        )
+    student = Student.objects.all()
+    ctx = {'students': student}
+    return render(request, 'students/student-list.html', ctx)
